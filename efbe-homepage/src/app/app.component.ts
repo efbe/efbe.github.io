181e680b8956e600b7a88a5dfcd60627fdb1ed6f
Balazs Foky <efbe@users.noreply.github.com>
2023-01-05 17:14:45 +0100

    Add Angular-CLI-GHpages

--- efbe-homepage/src/app/app.component.ts
+++ efbe-homepage/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Balazs Foky — Senior UX Designer';
+  title = 'efbe-homepage';
 }
