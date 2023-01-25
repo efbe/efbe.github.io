189ce6c461035b155bfc9a60c1792344e5521508
Balazs Foky <efbe@users.noreply.github.com>
2023-01-25 18:53:52 +0100

    Carousel demo page ver. I

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,6 +11,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarouselCardsComponent } from './article-carousel-cards/carousel-cards.component';
+
+const routes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
@@ -23,6 +28,7 @@ import { CarouselCardsComponent } from './article-carousel-cards/carousel-cards.
     AppRoutingModule,
     NoopAnimationsModule,
     MatIconModule,
+    RouterModule.forRoot(routes, { useHash: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
